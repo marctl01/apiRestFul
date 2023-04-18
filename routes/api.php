c103c2a2d9442f28be3c1db01b7cc66d281015f9
marctl01 <mtorrubia42@gmail.com>
2023-04-18 22:47:02 +0200

    Routes Crud Provider

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,9 @@ use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\PostController;
+use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\ProviderController;
+
 
 
 
@@ -22,32 +25,28 @@ use App\Http\Controllers\Api\PostController;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) { return $request->user(); });
 
-// Route::post('register', [AuthController::class, 'register']);
 
 // Autentificacion api
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
+// Pio toni
 Route::resource('post', PostController::class)->middleware(['auth:sanctum']);
 
 Route::middleware(['auth:sanctum'])->group(function(){
-    
     // CRUD Proveedor
-    Route::resource('/productos',ProductoController::class);
+        // CREATE
+            Route::post('products', [ProductController::class, 'store']); // Crear un nuevo producto
 
-    // CREATE
-        Route::post('products', 'ProductController@store'); // Crear un nuevo producto
-
-    // READ
-        Route::get('products', 'ProductController@index'); // Listar todos los productos
-        Route::get('products/{id}', 'ProductController@show'); // Mostrar un producto específico
-        
-    // UPDATE
-        Route::put('products/{id}', 'ProductController@update'); // Actualizar un producto
-        
-    // DELETE
-        Route::delete('products/{id}', 'ProductController@destroy'); // Eliminar un producto
-        
+        // READ
+            Route::get('products', [ProductController::class, 'index']); // Listar todos los productos
+            Route::get('products/{id}', [ProductController::class, 'show']); // Mostrar un producto específico
+            
+        // UPDATE
+            Route::put('products/{id}', [ProductController::class, 'update']); // Actualizar un producto
+            
+        // DELETE
+            Route::delete('products/{id}', [ProductController::class, 'destroy']); // Eliminar un producto
 
 });
 
